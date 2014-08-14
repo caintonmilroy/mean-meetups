53e1e14c7dcf34253368f2aa08acfc4b89608e5d
Steve Purves <stevejpurves@gmail.com>
2014-08-14 09:02:56 +0100

    added MeetupServer service

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,29 @@
 var meetup = angular.module('meetup', []);
 
-meetup.controller('meetupController',["$scope", function($scope){
-	$scope.meetups =[
-		{name:"Cainton"},
-		{name:"Milroy"}
-	]
-	$scope.addMeetup = function(){
-		console.log($scope.name);
-		if($scope.name){
-			$scope.meetups.push({name:$scope.name})
-		}
+meetup.factory('MeetupServer', [ function() {	
+	var meetups =  [{name:"Cainton"}, {name:"Milroy"}];
 
-	}
+	return { 
+		list: function() { 
+			var meetups_copy = [];
+			for ( var i = 0; i < meetups.length; i++) {
+				meetups_copy.push( meetups[i] );
+			}
+			return meetups_copy; 
+		},
+		add: function(meetup) { meetups.push(meetup) } 
+	};
+ }]);
+
+meetup.controller('meetupController',["$scope", "MeetupServer", function($scope, MeetupServer){
+	$scope.meetups = MeetupServer.list();
+
+	$scope.addMeetup = function(){
+		if($scope.name){
+			var meetup = {name:$scope.name};
+			$scope.meetups.push(meetup);
+			MeetupServer.add(meetup)
+			$scope.name = "";
+		}
+	};
 }]);
