8fee0cca4bc48723ccf80f59587b10433585180d
Steve Purves <stevejpurves@gmail.com>
2014-08-28 09:04:56 +0100

    switched to mocha reporting style

--- karma.conf.js
+++ karma.conf.js
@@ -35,8 +35,9 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
+    plugins: ['karma-jasmine','karma-mocha-reporter','karma-chrome-launcher'],
 
     // web server port
     port: 9876,
